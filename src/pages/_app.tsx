c566c0dea4b8817a447684081cc376784f8a47b9
Adrian Braga <adrian.braga.ro@gmail.com>
2021-05-27 20:37:32 -0400

    feat: estabelecer configuração do chakra UI #6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import * as React from "react";
 import { AppProps } from "next/app";
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <ChakraProvider>
+            <CSSReset />
             <Component {...pageProps} />
-        </>
+        </ChakraProvider>
     );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import {
+    Box,
+    Text
+} from '@chakra-ui/react';
 
 export default function Home() {
     return (
         <>
-            <h1>Hello Adopt a tree</h1>
+            <Box width="100%" height="100vh" display="flex" alignItems="center" justifyContent="center" flexDirection="row">
+                <Text 
+                    textAlign="center" 
+                    bgGradient="linear(to-l, #FF4F4F, #FFF)" 
+                    bgClip="text" 
+                    fontSize="50px"
+                    fontWeight="extrabold"
+                >
+                    Hello Adopt a Tree
+                </Text>
+            </Box>
         </>
     );
 }
