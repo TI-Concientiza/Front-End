fe41edfb88b5f52d6d5bd052a43fd7a7ccc0c128
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-06 22:27:03 -0400

    refactor: melhorando as cores da aplicação e do menu principal

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,21 +1,47 @@
+import { extendTheme } from "@chakra-ui/react";
 
-const theme = {
-  palette: {
-    primary: {
-      light: "#3f7352",
-      main: "#0F5027",
-      dark: "#0a381b",
-      contrastText: "#fff",
+const theme = extendTheme({
+    initialColorMode: "dark",
+    colors: {
+        global: {
+            bg: "#F2F7FC",
+            color: "#6E6E6E",
+        },
+        primary: {
+            light: "#3f7352",
+            main: "#0F5027",
+            dark: "#0a381b",
+            contrastText: "#fff",
+        },
+        secondary: {
+            light: "#a8c47a",
+            main: "#93B659",
+            dark: "#667f3e",
+            contrastText: "#6E6E6E",
+        },
+        error: {
+            main: "#c53030",
+        },
+        spotlight: {
+            main: '#FBE170'
+        }
     },
-    secondary: {
-      light: "#a8c47a",
-      main: "#93B659",
-      dark: "#667f3e",
-      contrastText: "#6E6E6E",
+    fonts: {
+        body: ["Roboto", '"Segoe UI"', "sans-serif"].join(","),
     },
-    error: {
-      main: "#c53030"
+    fontWeights: {
+        normal: 400,
+        medium: 600,
+        bold: 700,
+        xBold: 900,
     },
+});
+
+export default theme;
+
+/**
+ *
+ *   palette: {
     background: {
       default: '#F2F7FC',
       paper: '#ffff'
@@ -24,6 +50,5 @@ const theme = {
   typography: {
     fontFamily: ["Roboto", '"Segoe UI"', "sans-serif"].join(","),
   },
-};
-
-export default theme;
+ *
+ */
