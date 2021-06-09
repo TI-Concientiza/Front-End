cb03544d20fb03060da77e193172671326faf26a
Adrian Braga <adrian.braga.ro@gmail.com>
2021-06-09 13:11:30 -0400

    feat: adiciona o componente slideshow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,13 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+
 import Menu from "../components/Menu";
+import Slideshow from "../components/Slideshow";
 
 export default function Home() {
     return (
         <>
             <Menu />
-            <Box
-                width="100%"
-                height="100vh"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                flexDirection="row"
-            >
-                <Text
-                    textAlign="center"
-                    bgGradient="linear(to-l, #FF4F4F, #FFF)"
-                    bgClip="text"
-                    fontSize="50px"
-                    fontWeight="extrabold"
-                >
-                    Hello Adopt a Tree
-                </Text>
-            </Box>
+            <Slideshow />
         </>
     );
 }
