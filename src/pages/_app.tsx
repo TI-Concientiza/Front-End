3087716b2ba3f828d836126b01b5f223c9ca72d8
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-11 22:29:07 -0400

    style: corrigin problemas de estilização do código

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import { AppProps } from "next/app";
-import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import thema from "../utils/theme";
 
 import "../styles/global.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
     return (
         <ChakraProvider theme={thema}>
             <CSSReset />
             <Component {...pageProps} />
         </ChakraProvider>
     );
-}
+};
 
 export default MyApp;
